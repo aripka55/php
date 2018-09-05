00728dc6cd96bf43de72358c7ec6b7bf71ce4592
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-05 11:33:14 -0400

    update

---
+++ dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+    // Setting up the Database Connection
+    $db_host = 'localhost'; // Database is installed on PHP server
+    $db_user = 'andrew'; // Name to log into the database
+    $db_password = 'southhills#'; // Password to log into MySQL
+    $db_name = 'andrew'; // Name of the database within MySQL
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+    if ($conn->connect_server){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    ?>
