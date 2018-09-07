62ba012306790b22581f1caf0e3dda557dcf0b69
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-07 10:48:32 -0400

    update

--- register.php
+++ register.php
@@ -5,6 +5,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $password = $_POST['password'];
     $sql = "INSERT INTO users (username,password) VALUES ('$username, '$password')";
 }
+
 ?>
 <!DOCTYPE html>
 <html>
