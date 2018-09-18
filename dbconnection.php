cebc92e893ea5e075318bd0d9392de46b9df02df
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-18 10:39:03 -0400

    update

--- dbconnection.php
+++ dbconnection.php
@@ -5,7 +5,7 @@
     $db_password = 'southhills#'; // Password to log into MySQL
     $db_name = 'andrew'; // Name of the database within MySQL
     $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-    if ($conn->connect_server){
+    if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
     ?>
