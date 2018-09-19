69d6865aeb4394e6c59cfe8763350f1dca768fb3
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-19 11:25:38 -0400

    update

--- upload.php
+++ upload.php
@@ -32,7 +32,7 @@ if (isset($_FILES['upload']) ){ //could use != null after ] instead of isset
     mkdir("uploads/" . $_SESSION['username']);
   }
 
-  $target_dir = "uploads/";
+  $target_dir = "uploads/" . $_SESSION['username'];
   $target_file = $target_dir . basename($_FILES['upload']['name']);
   $uploadVerify = true;
 
