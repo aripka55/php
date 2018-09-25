318b49e1dd229d1f291b0b63252dd6ae0d9f9181
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-25 10:31:16 -0400

    update

---
+++ edituser.php
@@ -0,0 +1,24 @@
+<?php
+//Check to see if session has started
+if (!isset($_SESSION)){
+  session_start();
+}
+//If user is not logged in, send them to login page
+if (!isset($_SESSION['username'])){
+  header('Location: login.php');
+}
+
+if (isset($_GET['id']) && $_GET['edit']="edit"){
+    require('dbconnection.php'); // Bring in database connection
+    $sql = "SELECT * FROM users WHERE userid = " . $_GET['id'];
+    $result = $conn->query($sql);
+
+    echo "<form action=\"\" method=\"post\">"
+
+    while ($row = $result->fetch_assoc()){
+        echo "<input type=\"text\" disabled value=\"$row['userid']\">"
+    }
+    
+} else{
+    echo "You should net be here.";
+}
