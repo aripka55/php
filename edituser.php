a041b29b1b4fc16cf096920fee67a03ab551fdcd
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-25 10:33:51 -0400

    update

--- edituser.php
+++ edituser.php
@@ -1,25 +1,27 @@
 <?php
-//Check to see if session has started
 if (!isset($_SESSION)){
   session_start();
 }
-//If user is not logged in, send them to login page
+
 if (!isset($_SESSION['username'])){
   header('Location: login.php');
 }
 
-if (isset($_GET['id']) && $_GET['edit']="edit"){
-    require('dbconnection.php'); // Bring in database connection
-    $sql = "SELECT * FROM users WHERE userid = " . $_GET['id'];
-    $result = $conn->query($sql);
+if (isset($_GET['id']) && $_GET['edit']=="edit") {
+  require('dbconnection.php'); //bring in database connection
+  $sql = "SELECT * from users where userid = " . $_GET['id']; //id is int datatype don't quote it
+  $result = $conn->query($sql);
 
-    echo "<form action=\"\" method=\"post\">"
+  echo "<form action=\"\" method=\"post\">";
 
-    while ($row = $result->fetch_assoc()){
-        echo "<input type=\"text\" disabled value=\"$row['userid']\">"
-    }
+  while ($row = $result->fetch_assoc()) {
+    echo "<input type=\"text\" disabled value=\"" . $row['userid'] . "\">";
 
-} else{
-    echo "You should net be here.";
+  }
+
+
+} else {
+  echo "You should not be here.";
 }
+
 ?>
