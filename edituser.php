851cc64e6b5e39533344d2be36a0903c1882737f
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-09-26 10:15:04 -0400

    update

--- edituser.php
+++ edituser.php
@@ -1,32 +1,33 @@
 <?php
-if (!isset($_SESSION)){
+// check to see if session is started
+if (!isset($_SESSION)) {
   session_start();
 }
-
-if (!isset($_SESSION['username'])){
-  header('Location: login.php');
+//if username not logged in, will move them to login page
+if (!isset($_SESSION['username'])) {
+   header('Location: login.php');
 }
 
-if (isset($_GET['id']) && $_GET['edit']=="edit") {
-  require('dbconnection.php'); //bring in database connection
-  $sql = "SELECT * from users where userid = " . $_GET['id']; //id is int datatype don't quote it
-  $result = $conn->query($sql);
+if (isset($_GET['id']) && $_GET['edit']=="edit"){
 
-  echo "<form action=\"\" method=\"post\">";
+    require('dbconnection.php'); //bring in the database connection
+    $sql = "SELECT * from users where userid =" . $_GET['id']; // id is int datatype note don't quote it
+    $result = $conn->query($sql);
 
-  while ($row = $result->fetch_assoc()) {
-    echo "<input name=\"userid\" type=\"text\" disabled value=\"" . $row['userid'] . "\">";
-    echo "<br />";
-    echo "<input name=\"username\" type=\"text\"  value=\"" . $row['username'] . "\">";
-    echo "<br />";
-    echo "<input name=\"password\" type=\"text\"  value=\"" . $row['password'] . "\">";
-    echo "<br /">;
-    echo "<input type=\"submit\" name=\"submit\">";
-  }
+    echo "<form action=\"\" method=\"post\">";
 
+    while ($row = $result->fetch_assoc()) {
+        echo "<input name =\"userid\" type=\"text\" disabled value =\"". $row['userid'] ."\">";
+        echo "<br />";
+        echo "<input name =\"username\" type=\"text\" disabled value =\"". $row['username'] ."\">";
+        echo "<br />";
+        echo "<input name =\"password\"  type=\"text\" disabled value =\"". $row['password'] ."\">";
+        echo "<br />";
+        echo "<input type=\"submit\" name=\"submit\" value=\"change\">";
+    }
+
+    echo "</form>";
 
 } else {
-  echo "You should not be here.";
+    echo "You should not be here.";
 }
-
-?>
