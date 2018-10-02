0c400b90013019780c2840352c226d8cabc152ab
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-02 09:17:57 -0400

    update

--- navbar.php
+++ navbar.php
@@ -1,14 +1,18 @@
 <?php
-echo "<br />";
-//echo "<a href=users.php>Users</a>";
-
-echo (basename($_SERver['PHP_SELF']) == "users.php") ?" <a href=users.php><strong>Users</strong></a>" : "<a href=users.php>Users</a>";
-
-if (basename($_SERVER['PHP_SELF']) == "users.php"){
-    echo "<a href=users.php><strong>Users</strong></a>";
-} else{
-    "<a href=users.php>Users</a>";
+// checks to make sure session is started, if not, starts session
+if(!isset($_SESSION)){
+  session_start();
 }
 
-"<a href=upload.php>Upload</a>";
-?>
+ echo basename($_SERVER['PHP_SELF']) == "login.php"? "<a href=\"login.php\"><strong>Login/Logout</strong></a>" : "<a href=\"login.php\">Login/Logout</a>";
+ echo " | ";
+ echo basename($_SERVER['PHP_SELF']) == "register.php"? "<a href=\"register.php\"><strong>Register</strong></a>" : "<a href=\"register.php\">Register</a>";
+
+//these options only appear if user is logged in
+ if(isset($_SESSION['username'])){
+   echo " | ";
+   echo basename($_SERVER['PHP_SELF']) == "upload.php"? "<a href=\"upload.php\"><strong>Upload an Image</strong></a>" : "<a href=\"upload.php\">Upload an Image</a>";
+   echo " | ";
+   echo basename($_SERVER['PHP_SELF']) == "users.php"? "<a href=\"users.php\"><strong>View Site Users</strong><a/>" : "<a href=\"users.php\">View Site Users<a/>";
+ }
+  ?>
