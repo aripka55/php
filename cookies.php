f74d6ecaba3b7867451bff4d9e2ee8e46e1bfa7f
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-04 10:42:47 -0400

    update

--- cookies.php
+++ cookies.php
@@ -22,8 +22,8 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
     } else {
         echo "This is your first time here";
         //setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-        setcookie($cookie_name, $cookie_value, time() + (60), "/");
     }
+    setcookie($cookie_name, $cookie_value, time() + (60), "/");
     ?>
 </body>
 </html>
