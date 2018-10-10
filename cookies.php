35a64522acd3eb91e427488b44989922c1e08641
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-10 11:21:32 -0400

    update

--- cookies.php
+++ cookies.php
@@ -1,30 +1,17 @@
 <?php
 $cookie_name = "last_visit";
-$cookie_value = date("l jS \of F Y h:i:s A");// l -day of the week
-$seconds = $lastVisit / 60;
+$cookie_value = date("l jS \of F Y h:i:s A");
 
-//setcookie($cookie_name,$cookie_value, time() + (86400*30), "/");
-//86400 = 1 day
-//$inTwoMonths = 60 * 60 * 24 * 60 + time();
-//setcookie('user', date("G:i - m/d/y"), $inTwoMonths);
-//this adds one year to the current time, for the cookie expiration
-//$year = 31536000 + time() ;
-//setcookie(user, time (), $year);
-
-// Checking if the cookie is set
-if (isset($_COOKIE['last_visit'])) {
-  $notification = "You have been here before.";
+if (isset($_COOKIE['last_visit'])){
+  $notification = "You have been here within the last 30 days";
   $last_visit = $_COOKIE['last_visit'];
   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 } else {
-  $notification = "This is your first time visiting";
+  $notification = "This is your first time here, or it's been more than 30 days";
   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 }
 
-if ($seconds <= 60) {
-  $notification = "The last time you visited was " . (time() $last_visit) . " seconds ago";
-}
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -33,11 +20,13 @@ if ($seconds <= 60) {
     <title></title>
   </head>
   <body>
-    <p>
+    <h2>
       <?php
-          echo $notification;
-          echo ($last_visit != "")? "<br /> Last time you visited was: " . $last_visit : "";
-       ?>
-    </p>
+      echo $notification;
+      echo ($last_visit != "")? "<br /> Last Visit: " . $last_visit : "";
+      echo "<br /> " . time();
+      ?>
+
+    </h2>
   </body>
 </html>
