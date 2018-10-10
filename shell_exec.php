cc75c3b42d8073e1a7532d438a8c858467a31c4a
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-10 11:31:39 -0400

    update

--- shell_exec.php
+++ shell_exec.php
@@ -7,7 +7,7 @@ echo "<pre>$pwd</pre>";
 ?>
 
 <?php
-$filename = "/var/www/html/andrew/php";
+$filename = "/var/www/html/andrew/php/test";
 
 if (file_exists($filename)) {
     echo "$filename does exist";
