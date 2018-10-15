2d9c01e33b08f6c3a8144d021a00b7aa41c3e20e
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-15 11:09:15 -0400

    update

--- shell_exec.php
+++ shell_exec.php
@@ -6,24 +6,33 @@ $pwd = shell_exec('pwd');
 echo "<pre>$pwd</pre>";
 
 $file_test = file_exists("test");
-  if ($file_test) {
-    $folder_test = is_dir("test");
-    if ($folder_test) {
-      echo "test exists, and is a folder";
+if ($file_test) {
+  $folder_test = is_dir("test");
+  if ($folder_test) {
+    echo "test exists, and is a folder" . "<br/>";
 
-      $testArray = scandir("test/");
-      //var_dump($testArray);
+    $testArray = scandir("test/");
+    //var_dump($testArray);
 
-      foreach ($testArray as $key => $value) {
-        // code...
-        if ($value == "." || $value == "..") { continue; }
-        echo $value . "<br />";
-      }
-
-    } else {
-      echo "test exists and is a file";
+    foreach ($testArray as $key => $value) {
+      // code...
+      if ($value == "." || $value == "..") { continue; }
+      echo $value . "<br />";
     }
+
   } else {
-    mkdir("test");
+    echo "test exists and is a file";
   }
+} else {
+  mkdir("test");
+}
+
+$users = shell_exec("w");
+$usersExploded = explode("\n", $users);
+//var_dump($usersExploded);
+foreach ($usersExploded as $key => $value) {
+  if ($key == "0" || $key == "1") { continue; }
+  echo $value . "<br>";
+}
+
 ?>
