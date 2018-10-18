30feb1e3b82f7adaccddc10b638b723e933e4f40
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-18 10:27:18 -0400

    update

--- Follow Me/examples/profile.php
+++ Follow Me/examples/profile.php
@@ -39,6 +39,9 @@
 	            <ul class="navbar-nav ml-auto">
 	                <li class="nav-item">
 	                    <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+	                    <a href="login.php" class="nav-link">Login</a>
 	                </li>
 	            </ul>
 	        </div>
