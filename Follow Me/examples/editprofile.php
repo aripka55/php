7caa4f61979d48b8d873bfc25583b5ddb83b9b48
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-24 11:23:19 -0400

    update

--- Follow Me/examples/editprofile.php
+++ Follow Me/examples/editprofile.php
@@ -1,106 +1,121 @@
 <?php
-// Start Session if it is not running
-session_start();
-require('dbconnection.php');
+//Start Session if it is not running
+//Add name attributes to form elements
+//Set default values for each form element from $_SESSION
+//Update submitted values to database
+//Upldate submitted values to $_SESSION
 ?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Follow Me by Andrew</title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <!--Bootstrap core CSS-->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+        <title>Follow me by Matthew</title>
 
-    <!--CSS for Demo Purpose, don't include it in your project-->
-    <link href="../assets/css/demo.css" rel="stylesheet" />
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta name="viewport" content="width=device-width" />
 
-    <!--Fonts and icons-->
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+        <!-- Bootstrap core CSS     -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+
+        <!--  CSS for Demo Purpose, don't include it in your project     -->
+        <link href="../assets/css/demo.css" rel="stylesheet" />
+
+    <!--     Fonts and icons     -->
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
-        <div class="container">
-            <div class="navbar-translate">
-                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-bar"></span>
-                    <span class="navbar-toggler-bar"></span>
-                    <span class="navbar-toggler-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Follow Me</a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a href="login.php" class="nav-link">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <?php echo $_SESSION['email']; ?>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+    <div class="container">
+                        <div class="navbar-translate">
+               <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                             <span class="navbar-toggler-bar"></span>
+                                   <span class="navbar-toggler-bar"></span>
+                                   <span class="navbar-toggler-bar"></span>
+               </button>
+               <a class="navbar-brand" href="#">Follow Me</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="navbarToggler">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="login.php" class="nav-link">Login</a>
+                        </li>
+                                                                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                                                                                <?php echo $_SESSION['email']; ?>
+                                                                                        </a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
     </nav>
 
     <div class="wrapper">
-        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-            <div class="filter"></div>
-        </div>
-        
-        <!-- Keep in touch message for website -->
-        <div class="section landing-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 ml-auto mr-auto">
-                        <h2 class="text-center">Keep in touch?</h2>
-                        <form class="contact-form">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Name</label>
-                                    <div class="input-group">
-	                                    <span class="input-group-addon">
-	                                        <i class="nc-icon nc-single-02"></i>
-                                        </span>
-                                        <input type="text" class="form-control" placeholder="Name">
-	                                </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label>Email</label>
-                                    <div class="input-group">
-										<span class="input-group-addon">
-											<i class="nc-icon nc-email-85"></i>
-                                        </span>
-                                        <input type="text" class="form-control" placeholder="Email">
-									</div>
-                                </div>
+      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+                          <div class="filter"></div>
+                  </div>
+
+      <div class="section landing-section">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-8 ml-auto mr-auto">
+                      <h2 class="text-center">Edit Profile</h2>
+                      <form class="contact-form" action="" method="post">
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <label>First Name:</label>
+                                  <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="nc-icon nc-single-02"></i>
+                                    </span>
+                                    <input type="text" class="form-control" placeholder="First Name">
+                                  </div>
+                              </div>
+
+                              <div class="col-md-6">
+                                  <label>Last Name:</label>
+                                  <div class="input-group">
+                                    <span class="input-group-addon">
+                                      <i class="nc-icon nc-single-02"></i>
+                                    </span>
+                                    <input type="text" class="form-control" placeholder="Last Name">
+                                  </div>
+                              </div>
+                          </div> <!--Ends First Row -->
+
+
+                          <label>Title:</label>
+                          <div class="input-group">
+                            <span class="input-group-addon">
+                              <i class="nc-icon nc-tag-content"></i>
+                            </span>
+                            <input type="text" class="form-control" placeholder="Title">
+                          </div>
+
+                          <label>Description:</label>
+                          <textarea class="form-control" rows="4" placeholder="Tell everyone a little about you..."></textarea>
+                          <div class="row">
+                            <div class="col-md-4 ml-auto mr-auto text-center">
+                              <button class="btn btn-danger btn-lg btn-fill">Update</button>
                             </div>
-                            <label>Description</label>
-                            <textarea class="form-control" rows="4" placeholder="Tell everyone a little about you..."></textarea>
-                            <div class="row">
-                                <div class="col-md-4 ml-auto mr-auto">
-                                    <button class="btn btn-danger btn-lg btn-fill">Update</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>    
-    <footer class="footer section-dark">
+                          </div>
+
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+    </div>
+
+        <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -123,11 +138,12 @@ require('dbconnection.php');
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
-
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
+
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
+
 </html>
