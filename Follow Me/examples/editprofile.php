6a77385e504c17019cf35c58216f29eeae8c92f9
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-24 11:34:30 -0400

    update

--- Follow Me/examples/editprofile.php
+++ Follow Me/examples/editprofile.php
@@ -111,8 +111,8 @@
             </div>
         </div>
     </div>
-
-        <footer class="footer section-dark">
+    
+    <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -135,10 +135,10 @@
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-
 
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
