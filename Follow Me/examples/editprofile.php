563a5f72e2d5ebc20a527d4e0f75864bf3e358f9
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-25 10:21:34 -0400

    update

--- Follow Me/examples/editprofile.php
+++ Follow Me/examples/editprofile.php
@@ -2,17 +2,6 @@
 // Start Session if it is not running
 session_start();
 require("db_connection.php");
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db_host = 'localhost'; 
-    $db_user = 'andrew'; 
-    $db_password = 'southhills#'; 
-    $db_name = 'andrew'; 
-    $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
-
-    if ($conn->connect_error){ 
-        die("Connection failed: ". $conn->connect_error);
-    }
-        
     $sql ="UPDATE fm_users SET firstname='".$_POST['firstname']."', lastname='".$_POST['lastname']."', title='".$_POST['title']."', descr='".$_POST['descr']."' WHERE userid = " . $_SESSION['userid'];
     $result = $conn->query($sql);
     $sql="SELECT * FROM fm_users WHERE userid = " . $_SESSION['userid'];
