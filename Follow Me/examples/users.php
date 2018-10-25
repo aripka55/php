74a9988c8acaba19bba4edb65479b6d306eab11a
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-25 10:46:35 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -18,31 +18,29 @@ if (!isset($_SESSION)){
 //Set default values for each form element from $_SESSION
 //Update submitted values to SQLiteDatabase
 //Update submitted value to $_SESSION
-if (isset($_SESSION['email']) && isset($_POST['savebutton']))   {
-        $first_name=$_POST['first_name'];
-        $last_name=$_POST['last_name'];
-        $title=$_POST['title'];
-        $description=$_POST['description'];
+if (isset($_SESSION['email']) && isset($_POST['savebutton'])) {
+    $first_name=$_POST['first_name'];
+    $last_name=$_POST['last_name'];
+    $title=$_POST['title'];
+    $description=$_POST['description'];
 
-        $email=$_SESSION['email'];
-        require('sitedbconn.php');
-        $updatedb="UPDATE fm_users SET first_name=\"" .  $first_name . "\", last_name=\"" . $last_name .  "\", title=\"" . $title . "\", description=\"" . $description . "\" WHERE email = \"" . $email . "\"";
-
-
-        $conn->query($updatedb);
-
+    $email=$_SESSION['email'];
+    require('sitedbconn.php');
+    $updatedb="UPDATE fm_users SET first_name=\"" .  $first_name . "\", last_name=\"" . $last_name .  "\", title=\"" . $title . "\", description=\"" . $description . "\" WHERE email = \"" . $email . "\"";
+    
+    $conn->query($updatedb);
         $sql = "SELECT * FROM fm_users WHERE email = '$email'";
         $result = $conn->query($sql);
 
-         while ($row = $result->fetch_assoc()){
-                         //$_SESSION['email'] = $email;
-                         $_SESSION['first_name'] = $row['first_name'];
-                         $_SESSION['last_name'] = $row['last_name'];
-                         $_SESSION['description'] = $row['description'];
-                         $_SESSION['title'] = $row['title'];
-                         //$_SESSION['image_url'] = $row['image_url'];
-                 }
-header('Location: profile.php');
+        while ($row = $result->fetch_assoc()){
+            //$_SESSION['email'] = $email;
+            $_SESSION['first_name'] = $row['first_name'];
+            $_SESSION['last_name'] = $row['last_name'];
+            $_SESSION['description'] = $row['description'];
+            $_SESSION['title'] = $row['title'];
+            //$_SESSION['image_url'] = $row['image_url'];
+        }
+        header('Location: profile.php');
 }
 ?>
 <!doctype html>
