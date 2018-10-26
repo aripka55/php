2b7c5d80999c46e658700bcccb16ae7c65affd2c
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-26 17:37:31 -0400

    update

--- Follow Me/examples/profile.php
+++ Follow Me/examples/profile.php
@@ -1,18 +1,13 @@
 <?php
-//start session
-session_start();
-require('dbconnection.php');
-//uses $_SESSION['email'] to display navigation
+session_start(); //Start session_start
 
-//Modify fm_users table to include profile image url -- load it to $_SESSION['image_url']
+//Uses the $_SESSION['email'] POST variable to display the email in the nav bar at the top (Welcome $_SESSION['image_url'])
 
-//Modify fm_users table to include first_name, last_name $_SESSION['first_name'] ['last_name']
+//Modify fm_users to image_url...load it to the $_SESSION['email'] variable
 
-//Modify fm_users table to include a title $_SESSION['title']
-
-//Modify fm_users table to include a description $_SESSION['description']
-?>
-
+//Modify the fm_users table to include first and last name....Use the session variable first name and last name
+//Modify fm_users to add title and then load it to the $_SESSION['title']
+//Modify fm_users to add description and then load it to the $_SESSION['description']?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,22 +16,25 @@ require('dbconnection.php');
     <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
     <title>Follow Me by Andrew</title>
+
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-    <!-- Bootstrap core CSS     -->
+    <!-- Bootstrap core CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <!--  CSS for Demo Purpose, don't include it in your project -->
     <link href="../assets/css/demo.css" rel="stylesheet" />
 
-    <!--     Fonts and icons     -->
+    <!-- Fonts and icons -->
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
         <div class="container">
@@ -55,7 +53,9 @@ require('dbconnection.php');
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <?php echo $_SESSION['email']; ?>
+                            Welcome<?php
+                            echo " " . $_SESSION['email']; //WE need to use the session variable here because we don't have a variable called email on this page
+                            ?>
                         </a>
                     </li>
                 </ul>
@@ -71,91 +71,90 @@ require('dbconnection.php');
         <div class="section profile-content">
             <div class="container">
                 <div class="owner">
-                    <!--when user logs in, pull their image sql with url image
-                    MODIFY FM_USERS TABLE TO INCLUDE URL OF PROFILE PIC-->
-                    <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                </div>
-                <div class="name">
-                    <h4 class="title"><?php echo $_SESSION['first_name'] . "" . $_SESSION['last_name']; ?><br /></h4>
-                    <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 ml-auto mr-auto text-center">
-                    <p><?php echo $_SESSION['description']; ?> </p>
-                    <br />
-                    <btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
-                </div>
-            </div>
-            <br/>
-            <div class="nav-tabs-navigation">
-                <div class="nav-tabs-wrapper">
-                    <ul class="nav nav-tabs" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <!-- Tab panes -->
-            <div class="tab-content following">
-                <div class="tab-pane active" id="follows" role="tabpanel">
-                    <div class="row">
-                        <div class="col-md-6 ml-auto mr-auto">
-                            <ul class="list-unstyled follows">
-                                <li>
-                                    <div class="row">
-                                        <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                                            <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                        </div>
-                                        <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                            <h6>Flume<br/><small>Musical Producer</small></h6>
-                                        </div>
-                                        <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="checkbox" value="" checked>
-                                                    <span class="form-check-sign"></span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <hr />
-                                <li>
-                                    <div class="row">
-                                        <div class="col-md-2 ml-auto mr-auto ">
-                                            <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                        </div>
-                                        <div class="col-md-7 col-sm-4">
-                                            <h6>Banks<br /><small>Singer</small></h6>
-                                        </div>
-                                        <div class="col-md-3 col-sm-2">
-                                            <div class="form-check">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="checkbox" value="">
-                                                    <span class="form-check-sign"></span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                    <div class="avatar">
+                        <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                    </div>
+                    <div class="name">
+                        <h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4>
+                        <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
                     </div>
                 </div>
-                <div class="tab-pane text-center" id="following" role="tabpanel"> 
-                    <h3 class="text-muted">Not following anyone yet :(</h3>
-                    <button class="btn btn-warning btn-round">Find artists</button>
+                <div class="row">
+                    <div class="col-md-6 ml-auto mr-auto text-center">
+                        <p><?php echo $_SESSION['description']; ?></p>
+                        <br />
+                        <btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
+                    </div>
+                </div>
+                <br/>
+                <div class="nav-tabs-navigation">
+                    <div class="nav-tabs-wrapper">
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Tab panes -->
+                <div class="tab-content following">
+                    <div class="tab-pane active" id="follows" role="tabpanel">
+                        <div class="row">
+                            <div class="col-md-6 ml-auto mr-auto">
+                                <ul class="list-unstyled follows">
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                                <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                            </div>
+                                            <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                                <h6>Flume<br/><small>Musical Producer</small></h6>
+                                            </div>
+                                            <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                                <div class="form-check">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="" checked>
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <hr />
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-md-2 ml-auto mr-auto ">
+                                                <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                            </div>
+                                            <div class="col-md-7 col-sm-4">
+                                                <h6>Banks<br /><small>Singer</small></h6>
+                                            </div>
+                                            <div class="col-md-3 col-sm-2">
+                                                <div class="form-check">                                                                        
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="">
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane text-center" id="following" role="tabpanel">
+                        <h3 class="text-muted">Not following anyone yet :(</h3>
+                        <button class="btn btn-warning btn-round">Find artists</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <footer class="footer section-dark">
+        <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
