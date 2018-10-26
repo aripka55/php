7753f68e765882a65f4a610a9b454db768a579da
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-26 17:30:48 -0400

    update

--- Follow Me/examples/editprofile.php
+++ Follow Me/examples/editprofile.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['email']) && isset($_POST['savebutton']))   {
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>Follow Me Into the Darkness</title>
+    <title>Follow Me by Andrew</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
