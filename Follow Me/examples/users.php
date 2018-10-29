a4f10a63f5fa063771161fe0b9368494f7810e79
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-29 08:14:09 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -123,10 +123,10 @@ if (!isset($_SESSION)){
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-
 
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
