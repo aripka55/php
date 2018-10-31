5fd1e9b8199851af8ac6980da61634a1c2d8630d
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-31 17:59:51 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -17,107 +17,118 @@ while($row = $follow_result->fetch_row()) {
 }
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>Follow me by Andrew</title>
+        <title>Follow me by Aaron</title>
 
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta name="viewport" content="width=device-width" />
 
-    <!-- Bootstrap core CSS     -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+        <!-- Bootstrap core CSS     -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="../assets/css/demo.css" rel="stylesheet" />
+        <!--  CSS for Demo Purpose, don't include it in your project     -->
+        <link href="../assets/css/demo.css" rel="stylesheet" />
 
     <!--     Fonts and icons     -->
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
-        <div class="container">
-            <div class="navbar-translate">
-                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-bar"></span>
-                    <span class="navbar-toggler-bar"></span>
-                    <span class="navbar-toggler-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Follow Me</a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a href="login.php" class="nav-link">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <?php echo $_SESSION['email']; ?>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+    <div class="container">
+                        <div class="navbar-translate">
+               <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                             <span class="navbar-toggler-bar"></span>
+                                   <span class="navbar-toggler-bar"></span>
+                                   <span class="navbar-toggler-bar"></span>
+               </button>
+               <a class="navbar-brand" href="#">Follow Me</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="navbarToggler">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="login.php" class="nav-link">Login</a>
+                        </li>
+                                                                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                                                                                <?php echo $_SESSION['email']; ?>
+                                                                                        </a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
     </nav>
+
     <div class="wrapper">
-        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/default.jpg');">
-            <div class="filter">
-            </div>
-        </div>
-        <br />
-        <br />
-        <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-                <ul class="list-unstyled follows">
-                    <?php
-                    while($row = $result->fetch_assoc()) {
-                        $user_id = $row['user_id'];
+      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+                          <div class="filter"></div>
+                  </div>
 
-                        if ($user_id == $user_id){
-                        }
-                        else {
-                            echo "<li>";
-                            echo "<div class=\"row\">";
-                            echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">";
-                            echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                            echo "</div>";
-                            echo "<div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
-                            echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                            echo "</div>";
-                            echo "<div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">";
-                            echo "<div class=\"form-check\">";
-                            echo "<label class=\"form-check-label\">";
-                            echo "<input class=\"form-check-input\" type=\"checkbox\" value=\"\"";
-                                
-                            if (in_array($user_id, $following_user_id)) {
-                                echo " checked";
-                            }
-                            echo ">";
-                            echo "<span class=\"form-check-sign\"></span>";
-                            echo "</label>";
-                            echo "</div>";
-                            echo "</div>";
-                            echo "</div>";
-                            echo "</li>";
-                            echo "<hr />";
-                        }
-                    }
-                    ?>
-                </ul>
-            </div>
-        </div>
-    </div>
+                        <br />
+                        <br />
 
-    <footer class="footer section-dark">
+                        <div class="row">
+                                <div class="col-md-6 ml-auto mr-auto">
+                                        <ul class="list-unstyled follows">
+                                                <?php
+
+            while($row = $result->fetch_assoc()) {
+
+              $user_id = $row['user_id'];
+
+              if ($user_id == $userid){
+
+              }
+              else {
+
+              echo "<li>";
+                                                echo    "<div class=\"row\">";
+                                                echo            "<div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">";
+                                                echo                    "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
+                                                echo            "</div>";
+                                                echo            "<div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
+                                                echo                    "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
+                                                echo            "</div>";
+                                                echo            "<div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">";
+                                                echo                    "<div class=\"form-check\">";
+                                                echo                            "<label class=\"form-check-label\">";
+                                                echo                                    "<input class=\"form-check-input\" type=\"checkbox\" value=\"\"";
+
+              if (in_array($user_id, $following_user_ids)) {
+
+                echo " checked";
+              }
+              echo ">";
+
+                                                echo                                    "<span class=\"form-check-sign\"></span>";
+                                                echo                            "</label>";
+                                                echo                    "</div>";
+                                                echo            "</div>";
+                                                echo    "</div>";
+                                                echo "</li>";
+              echo "<hr />";
+
+              }
+            }
+                                                ?>
+
+                                        </ul>
+                                </div>
+                        </div>
+                </div>
+
+
+        <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -140,12 +151,12 @@ while($row = $follow_result->fetch_row()) {
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
-
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
 
-
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
+
+</html>
