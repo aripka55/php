b7926dbbfb17bec17ecbe03ae70236ef874eb3be
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-31 18:06:22 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -20,15 +20,15 @@ while($row = $follow_result->fetch_row()) {
 <!doctype html>
 <html lang="en">
 <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Follow me by Andrew</title>
+    <title>Follow me by Andrew</title>
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  <meta name="viewport" content="width=device-width" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
 
         <!-- Bootstrap core CSS     -->
         <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
