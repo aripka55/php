5780d51d78c8617c0f78c6b87ae4cf7603da3ab5
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-31 18:00:30 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -25,7 +25,7 @@ while($row = $follow_result->fetch_row()) {
         <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Follow me by Aaron</title>
+        <title>Follow me by Andrew</title>
 
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
   <meta name="viewport" content="width=device-width" />
