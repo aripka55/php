c1e4191485f9d3feed428cce386d9dcd5b3352f2
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-10-31 11:14:34 -0400

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -10,69 +10,68 @@ $qresult = $conn->query($followers);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Follow me by Andrew</title>
+    <title>Follow me by Andrew</title>
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  <meta name="viewport" content="width=device-width" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
 
-        <!-- Bootstrap core CSS     -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
 
     <!--     Fonts and icons     -->
-        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
-
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
-  <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
-    <div class="container">
-                        <div class="navbar-translate">
-               <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                             <span class="navbar-toggler-bar"></span>
-                                   <span class="navbar-toggler-bar"></span>
-                                   <span class="navbar-toggler-bar"></span>
-               </button>
-               <a class="navbar-brand" href="#">Follow Me</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a href="login.php" class="nav-link">Login</a>
-                        </li>
-                                                                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                                                                                <?php echo $_SESSION['email']; ?>
-                                                                                        </a>
-                        </li>
-                    </ul>
-                </div>
-                </div>
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+        <div class="container">
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Follow Me</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <?php echo $_SESSION['email']; ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <div class="wrapper">
-      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-                          <div class="filter"></div>
-                  </div>
-
-                        <br />
-                        <br />
-
-                        <div class="row">
-                                <div class="col-md-6 ml-auto mr-auto">
-                                        <ul class="list-unstyled follows">
-                                          <?php
-                                          require('sitedbconn.php');
-                                          $sql = "SELECT * FROM fm_users";
+        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+            <div class="filter">
+            </div>
+        </div>
+        <br />
+        <br />
+        <div class="row">
+            <div class="col-md-6 ml-auto mr-auto">
+                <ul class="list-unstyled follows">
+                    <?php
+                    require('dbconnection.php');
+                    $sql = "SELECT * FROM fm_users";
 
                                           //Execute the SQL Query
                                           $result = $conn->query($sql);
