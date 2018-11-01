e6b109a760a14dff20479cbcc610d53cd0533ccc
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-01 18:54:27 -0400

    update

--- Follow Me/examples/profile.php
+++ Follow Me/examples/profile.php
@@ -1,103 +1,81 @@
 <?php
-// start session
-// uses $_SESSION['email'] to display email in navigation
-// modify fm_users to include image URL, load it to $_SESSION['image_url'];
-// modify the fm_users table to include first_name and last_name
-// modify fm_users to add title, load it to $_SESSION['title'];
-// modify fm_users to add description, load it to $_SESSION['description'];
-
 if (!isset($_SESSION)) {
-  session_start();
+    session_start();
 }
 require('dbconnection.php');
-
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Follow Me by Andrew</title>
+    <title>Follow Me by Andrew</title>
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-        <!-- Bootstrap core CSS     -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
 
-    <!--     Fonts and icons     -->
-        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
-
+    <!-- Fonts and icons -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
         <div class="container">
-                        <div class="navbar-translate">
-                    <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-bar"></span>
-                                        <span class="navbar-toggler-bar"></span>
-                                        <span class="navbar-toggler-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Follow Me</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a href="login.php" class="nav-link">Login</a>
-                        </li>
-                  <li class="nav-item">
-                     <a href="editprofile.php" class="nav-link">Edit Profile</a>
-                 </li>
-                 <li class="nav-item">
-                     <a href="follows.php" class="nav-link">Follows</a>
-                 </li>
-                                                                        <li class="nav-item">
-                            <a href="profile.php" class="nav-link">
-
-                                                                                                        <?php echo $_SESSION['email']; ?>
-
-                                                                                        </a>
-                        </li>
-                    </ul>
-                </div>
-                </div>
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Follow Me</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <?php echo $_SESSION['email']; ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <div class="wrapper">
         <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-                        <div class="filter"></div>
-                </div>
+            <div class="filter">
+            </div>
+        </div>
         <div class="section profile-content">
             <div class="container">
                 <div class="owner">
                     <div class="avatar">
-
                         <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-
                     </div>
                     <div class="name">
-
                         <h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4>
-
-                                                <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
-
+                        <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 ml-auto mr-auto text-center">
-
                         <p><?php echo $_SESSION['description']; ?></p>
-
                         <br />
                         <btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
                     </div>
@@ -115,6 +93,7 @@ require('dbconnection.php');
                         </ul>
                     </div>
                 </div>
+
                 <!-- Tab panes -->
                 <div class="tab-content following">
                     <div class="tab-pane active" id="follows" role="tabpanel">
@@ -130,12 +109,12 @@ require('dbconnection.php');
                                                 <h6>Flume<br/><small>Musical Producer</small></h6>
                                             </div>
                                             <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-                                                                                                <div class="form-check">
-                                                                        <label class="form-check-label">
-                                                                            <input class="form-check-input" type="checkbox" value="" checked>
-                                                                            <span class="form-check-sign"></span>
-                                                                        </label>
-                                                                    </div>
+                                                <div class="form-check">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="" checked>
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
                                             </div>
                                         </div>
                                     </li>
@@ -149,12 +128,12 @@ require('dbconnection.php');
                                                 <h6>Banks<br /><small>Singer</small></h6>
                                             </div>
                                             <div class="col-md-3 col-sm-2">
-                                                                                                <div class="form-check">
-                                                                        <label class="form-check-label">
-                                                                            <input class="form-check-input" type="checkbox" value="">
-                                                                            <span class="form-check-sign"></span>
-                                                                        </label>
-                                                                    </div>
+                                                <div class="form-check">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="">
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
                                             </div>
                                         </div>
                                     </li>
@@ -170,7 +149,7 @@ require('dbconnection.php');
             </div>
         </div>
     </div>
-        <footer class="footer section-dark">
+    <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -193,10 +172,10 @@ require('dbconnection.php');
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-
 
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
