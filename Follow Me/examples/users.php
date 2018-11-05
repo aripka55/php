4d8267014627c804cb1ce5b3246abf6461a0df2e
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-05 11:14:44 -0500

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -140,6 +140,7 @@ while($row = $follow_result->fetch_row()) {
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
