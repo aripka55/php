3873d4d1a75df0cb18478e6d09762a733e07eacf
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-05 11:15:15 -0500

    update

--- Follow Me/examples/users.php
+++ Follow Me/examples/users.php
@@ -145,7 +145,6 @@ while($row = $follow_result->fetch_row()) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
-
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
 
