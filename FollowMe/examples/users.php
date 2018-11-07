a5ce075a163b0ccafbaa40f8ef00cb9005fb721e
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-07 16:42:36 -0500

    update

--- FollowMe/examples/users.php
+++ FollowMe/examples/users.php
@@ -106,54 +106,48 @@ while($row = $follow_result->fetch_row()) {
         </div>
         <br />
         <br />
-        <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-                <ul class="list-unstyled follows">
-                    <form method="post" action="">
-                        <?php
-                        while($row = $result->fetch_assoc()) {
-                            $user_id = $row['user_id'];
-                            if ($user_id == $userid){
-                            }
-                            else {
-                                echo "<li>";
-                                echo "<div class=\"row\">";
-                                echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">";
-                                echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                echo "</div>";
-                                echo "<div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
-                                echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                echo "</div>";
-                                echo "<div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">";
-                                echo "<div class=\"form-check\">";
-                                echo "<label class=\"form-check-label\">";
-                                echo "<input class=\"form-check-input\" name=" . $row['first_name'] . " type=\"checkbox\" value=\"yes\"";
-
-                                if (in_array($user_id, $following_user_id)) {
-                                    echo " checked";
-                                }
-                                echo ">";
-                                echo "<span class=\"form-check-sign\"></span>";
-                                echo "</label>";
-                                echo "</div>";
-                                echo "</div>";
-                                echo "</div>";
-                                echo "</li>";
-                                echo "<hr />";
-                            }
-                        }
+        <form>
+            <div class="row">
+                <div class="col-md-6 ml-auto mr-auto">
+                    <ul class="list-unstyled follows">
+                        <?php while($row = $result->fetch_assoc()){ 
                         ?>
-                        <div class="row">
-                            <div class="col-md-4 ml-auto mr-auto text-center">
-                                <button class="btn btn-danger btn-lg btn-fill">Submit</button>
+                        <li>
+                            <div class="row">
+                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                    <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                </div>
+                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                    <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+                                        <br />
+                                        <small>
+                                            <?php echo $row['title'] ; ?>
+                                        </small>
+                                    </h6>
+                                </div>
+                                <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                    <div class="form-check">
+                                        <label class="form-check-label"><!--echo if checked only if followed -->
+                                            <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+                                            <span class="form-check-sign"></span>
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </ul>
+                        </li>
+                        <hr />
+                        <?php } ?>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-md-4 ml-auto mr-auto text-center">
+                    <button class="btn btn-danger btn-lg btn-fill">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
-
+        
     <footer class="footer section-dark">
         <div class="container">
             <div class="row">
