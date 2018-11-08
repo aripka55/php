9bf6bd75ca57da606ae6e541a3e7eb1c7b28ad3e
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-08 10:27:37 -0500

    update

--- FollowMe/examples/register.php
+++ FollowMe/examples/register.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>Follow Me by Andrew</title>
+    <title>Register Page</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
--- FollowMe/examples/users.php
+++ FollowMe/examples/users.php
@@ -46,7 +46,7 @@ while($row = $follow_result->fetch_row()) {
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>Follow Me by Andrew</title>
+    <title>Users Page</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
