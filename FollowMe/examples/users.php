596f9bc0a651727a5d2146c109e8b0713cd81d04
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-08 11:10:28 -0500

    update

--- FollowMe/examples/users.php
+++ FollowMe/examples/users.php
@@ -99,56 +99,54 @@ while($row = $follow_result->fetch_row()) {
     </nav>
 
     <div class="wrapper">
-      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-                          <div class="filter"></div>
-                  </div>~
-
-                        <br />
-                        <br />
-
-    <form action="" method="POST">
-
-                        <div class="row">
-                                <div class="col-md-6 ml-auto mr-auto">
-                                        <ul class="list-unstyled follows">
-                                                 <?php while($row = $result->fetch_assoc()){ ?>
-                                                <li>
-                                                        <div class="row">
-                                                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                                                        <!-- image-->   <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                                                </div>
-                                                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                                        <!--name-->             <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
-
-                                                        <!-- title-->   <br/><small><?php       echo $row['title'] ; ?></small></h6>
-                                                                </div>
-                                        <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-                                                                        <div class="form-check">
-                                                                <label class="form-check-label"><!--echo if checked only if followed -->
-                                                                <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
-                                                        <span class="form-check-sign"></span>
-                                                                                </label>
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-                                                </li>
-                                                <hr />
-                                        <?php } ?>
-                                        </ul>
+        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+            <div class="filter">
+            </div>
+        </div>
+        <br />
+        <br />
+        <form action="" method="POST">
+            <div class="row">
+                <div class="col-md-6 ml-auto mr-auto">
+                    <ul class="list-unstyled follows">
+                        <?php while($row = $result->fetch_assoc()){ ?>
+                        <li>
+                            <div class="row">
+                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                    <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
                                 </div>
-                        </div>
-
-
-      <div class="row">
-          <div class="col-md-4 ml-auto mr-auto text-center">
-              <button class="btn btn-danger btn-lg btn-fill">Submit</button>
-          </div>
-      </div>
-
-    </form>
+                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                    <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+                                        <br/>
+                                        <small>
+                                            <?php echo $row['title'] ; ?>
+                                        </small>
+                                    </h6>
+                                </div>
+                                <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                    <div class="form-check">
+                                        <label class="form-check-label"><!--echo if checked only if followed -->
+                                            <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+                                            <span class="form-check-sign"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <hr />
+                        <?php } ?>
+                    </ul>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 ml-auto mr-auto text-center">
+                    <button class="btn btn-danger btn-lg btn-fill">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
-        <footer class="footer section-dark">
+    <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -157,8 +155,25 @@ while($row = $follow_result->fetch_row()) {
                         <li><a href="http://blog.creative-tim.com">Blog</a></li>
                         <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
                     </ul>
-                </nav>
+                 </nav>
                 <div class="credits ml-auto">
                     <span class="copyright">
-                        © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
-                        </html>
+                        © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Creative Tem
+                    </span>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+<!-- Core JS Files -->
+<script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+<script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
+<!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Paper Kit Initialization snd functons -->
+<script src="../assets/js/paper-kit.js?v=2.1.0"></script>
+</html>
