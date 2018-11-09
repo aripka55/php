c1259cd5cf9667d28a1d58b02afca4032e7c4802
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-09 16:49:49 -0500

    update

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -1,19 +1,21 @@
 <?php
-if (!isset($_SESSION)) {
-    session_start();
-}
+session_start();
 require('dbconnection.php');
-$userid = $_SESSION['user_id'];
 
 $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+
 $result = $conn->query($sql);
 
-$follow_sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
-$follow_result = $conn->query($follow_sql);
+$userid = $_SESSION['user_id'];
+
+$sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
+
+$follow_result = $conn->query($sql);
 
 while($row = $follow_result->fetch_row()) {
-    $followingUserids[] = $row[0];
+    $following_user_id[] = $row[0];
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +35,7 @@ while($row = $follow_result->fetch_row()) {
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-    <!--  CSS for Demo Purpose, don't include it in your project -->
+    <!-- CSS for Demo Purpose, don't include it in your project -->
     <link href="../assets/css/demo.css" rel="stylesheet" />
 
     <!-- Fonts and icons -->
@@ -41,8 +43,8 @@ while($row = $follow_result->fetch_row()) {
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
-<body>
 
+<body>
     <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
         <div class="container">
             <div class="navbar-translate">
@@ -51,7 +53,7 @@ while($row = $follow_result->fetch_row()) {
                     <span class="navbar-toggler-bar"></span>
                     <span class="navbar-toggler-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Users</a>
+                <a class="navbar-brand" href="#">Edit Profile</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarToggler">
                 <ul class="navbar-nav ml-auto">
@@ -65,7 +67,7 @@ while($row = $follow_result->fetch_row()) {
                         <a href="editprofile.php" class="nav-link">Edit Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a href="users.php" class="nav-link">Users</a>
+                        <a href="users_old.php" class="nav-link">Users</a>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
@@ -105,7 +107,7 @@ while($row = $follow_result->fetch_row()) {
                     <div class="nav-tabs-wrapper">
                         <ul class="nav nav-tabs" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Followers</a>
+                                <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
@@ -113,65 +115,99 @@ while($row = $follow_result->fetch_row()) {
                         </ul>
                     </div>
                 </div>
+
                 <!-- Tab panes -->
                 <div class="tab-content following">
                     <div class="tab-pane active" id="follows" role="tabpanel">
                         <div class="row">
                             <div class="col-md-6 ml-auto mr-auto">
                                 <ul class="list-unstyled follows">
-                                    <?php
-                                        /*while($row = $result->fetch_assoc()) {
-                                            $following_userid = $row['following_user_id'];
-                        
-                                            if (in_array($following_userid, $user_ids)) {
-                                                echo "<li>";
-                                                echo "<div class=\"row\">";
-                                                echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-                                                echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                                echo "</div>";
-                                                echo "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-                                                echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                                echo "</div>";
-                                                echo "</div>";
-                                                echo "</li>";
-                                                echo "<hr />";
-                                            }
-                                        } */
-                                    ?>
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                                <img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                            </div>
+                                            <div class="col-md-7 col-sm-4 ml-auto mr-auto">
+                                                <h6>Flume<br/><small>Musical Producer</small></h6>
+                                            </div>
+                                            <div class="col-md-3 col-sm-2 ml-auto mr-auto">
+                                                <div class="form-check">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="" checked>
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <hr />
+                                    <li>
+                                        <div class="row">
+                                            <div class="col-md-2 ml-auto mr-auto ">
+                                                <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                            </div>
+                                            <div class="col-md-7 col-sm-4">
+                                                <h6>Banks
+                                                    <br />
+                                                    <small>Singer
+                                                    </small>
+                                                </h6>
+                                            </div>
+                                            <div class="col-md-3 col-sm-2">
+                                                <div class="form-check">
+                                                    <label class="form-check-label">
+                                                        <input class="form-check-input" type="checkbox" value="">
+                                                        <span class="form-check-sign"></span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
+                    <!--Following Section from Users Old-->
                     <div class="tab-pane text-center" id="following" role="tabpanel">
+                        <?php 
+                            while($row = $result->fetch_assoc()){ 
+                        ?>
                         <div class="row">
-                            <div class="col-md-6 ml-auto mr-auto">
-                                <ul class="list-unstyled follows">
-                                    <?php
-                                        while($row = $result->fetch_assoc()) {
-                                            $userid = $row['user_id'];
-                                            if (in_array($user_id, $followingUserids)) {
-                                                echo "<li>";
-                                                echo "<div class=\"row\">";
-                                                echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-                                                echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                                echo "</div>";
-                                                echo "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-                                                echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                                echo "</div>";
-                                                echo "</div>";
-                                                echo "</li>";
-                                                echo "<hr />";
-                                            }
-                                        }
+                            <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                            </div>
+                            <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                <h6>
+                                    <?php 
+                                        echo $row['first_name'] ." " . $row['last_name'] ; 
                                     ?>
-                                </ul>
+                                    <br/>
+                                    <small>
+                                        <?php       
+                                            echo $row['title'] ; 
+                                        ?>
+                                    </small>
+                                </h6>
+                            </div>
+                            <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                <div class="form-check">
+                                    <label class="form-check-label"><!--echo if checked only if followed -->
+                                        <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+                                        <span class="form-check-sign"></span>
+                                    </label>
+                                </div>
                             </div>
                         </div>
+                        <hr />
+                        <?php 
+                            } 
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <footer class="footer section-dark">
         <div class="container">
             <div class="row">
@@ -200,7 +236,7 @@ while($row = $follow_result->fetch_row()) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
-<!--  Paper Kit Initialization snd functons -->
+<!-- Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
 
 </html>
