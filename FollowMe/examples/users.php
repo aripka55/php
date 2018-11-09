6bde8b3aff96a80fcb651fcf776d78e13361cd59
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-09 17:07:15 -0500

    update

--- FollowMe/examples/users.php
+++ FollowMe/examples/users.php
@@ -1,34 +1,34 @@
 <?php
 if (!isset($_SESSION)) {
-  session_start();
+    session_start();
 }
 require('dbconnection.php');
 
-$userid = $_SESSION['user_id'];
+$userid= $_SESSION['user_id'];
 
 $sql2 = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
 $result2 = $conn->query($sql2);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     while ($row2 = $result2->fetch_assoc()) {
-        $firstName = $row2['first_name'];
-
-        if ($_POST["$firstName"] == "yes") {
-
+        $userId = $row2['user_id'];
+        if ($_POST["$userId"] == "yes") {
             $follow_id = $row2['user_id'];
-            $sql2 = "INSERT IGNORE INTO fm_followers (user_id, following_user_id) VALUES ('$userid','$follow_id')";
+            echo $follow_id . "=" . $userid;
+
+            $sql2 = "INSERT IGNORE INTO fm_followers(user_id, following_user_id) VALUES ('$userid','$follow_id')";
             $conn->query($sql2);
         }
         else {
             $follow_id = $row2['user_id'];
-            $sql2 = "DELETE FROM fm_followers WHERE user_id = '$userid' AND following_user_id = '$follow_id'";
+            $sql2 = "DELETE FROM fm_followers WHERE user_id = '$user_id' AND following_user_id = '$follow_id'";
             $conn->query($sql2);
         }
+        header('location: profile.php');
     }
-    header('Location: profile.php');
 }
 
-$sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+$sql = "SELECT * FROM fm_users";
 $result = $conn->query($sql);
 
 $sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
@@ -36,7 +36,7 @@ $sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
 $follow_result = $conn->query($sql);
 
 while($row = $follow_result->fetch_row()) {
-    $followingUserids[] = $row[0];
+    $following_user_id[] = $row[0];
 }
 ?>
 
@@ -53,11 +53,11 @@ while($row = $follow_result->fetch_row()) {
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-    <!-- Bootstrap core CSS     -->
+    <!-- Bootstrap core CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <!-- CSS for Demo Purpose, don't include it in your project -->
     <link href="../assets/css/demo.css" rel="stylesheet" />
 
     <!-- Fonts and icons -->
@@ -75,7 +75,7 @@ while($row = $follow_result->fetch_row()) {
                     <span class="navbar-toggler-bar"></span>
                     <span class="navbar-toggler-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Follow Me</a>
+                <a class="navbar-brand" href="#">Users</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarToggler">
                 <ul class="navbar-nav ml-auto">
@@ -89,7 +89,12 @@ while($row = $follow_result->fetch_row()) {
                         <a href="editprofile.php" class="nav-link">Edit Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a href="users.php" class="nav-link">Users</a>
+                        <a href="users_old.php" class="nav-link">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <?php echo $_SESSION['email']; ?>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -103,54 +108,55 @@ while($row = $follow_result->fetch_row()) {
         </div>
         <br />
         <br />
-        <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-                <ul class="list-unstyled follows">
-                    <form method="post" action="">
-                        <?php
-                            while($row = $result->fetch_assoc()) {
-                                $user_id = $row['user_id'];
-                                if ($user_id == $userid){
-                                }
-                                else {
-                                    echo "<li>";
-                                    echo "<div class=\"row\">";
-                                    echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">";
-                                    echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                    echo "</div>";
-                                    echo "<div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
-                                    echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                    echo "</div>";
-                                    echo "<div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">";
-                                    echo "<div class=\"form-check\">";
-                                    echo "<label class=\"form-check-label\">";
-                                    echo "<input class=\"form-check-input\" name=" . $row['first_name'] . " type=\"checkbox\" value=\"yes\"";
-
-                                    if (in_array($user_id, $followingUserids)) {
-                                        echo " checked";
-                                    }
-                                    echo ">";
-                                    echo "<span class=\"form-check-sign\"></span>";
-                                    echo "</label>";
-                                    echo "</div>";
-                                    echo "</div>";
-                                    echo "</div>";
-                                    echo "</li>";
-                                    echo "<hr />";
-                                }
-                            }
+        <form action="" method="POST">
+            <div class="row">
+                <div class="col-md-6 ml-auto mr-auto">
+                    <ul class="list-unstyled follows">
+                        <?php 
+                            while($row = $result->fetch_assoc()){ 
                         ?>
-                        <div class="row">
-                            <div class="col-md-4 ml-auto mr-auto text-center">
-                                <button class="btn btn-danger btn-lg btn-fill">Submit</button>
+                        <li>
+                            <div class="row">
+                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                                    <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                </div>
+                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                                    <h6>
+                                        <?php 
+                                            echo $row['first_name'] ." " . $row['last_name'] ; 
+                                        ?>
+                                        <br/>
+                                        <small>
+                                            <?php       
+                                                echo $row['title'] ; 
+                                            ?>
+                                        </small>
+                                    </h6>
+                                </div>
+                                <div class="col-md-3 col-sm-2  ml-auto mr-auto">
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            <input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+                                            <span class="form-check-sign"></span>
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </ul>
+                        </li>
+                        <hr />
+                        <?php 
+                            } 
+                        ?>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-md-4 ml-auto mr-auto text-center">
+                    <button class="btn btn-danger btn-lg btn-fill">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
-
     <footer class="footer section-dark">
         <div class="container">
             <div class="row">
@@ -179,6 +185,7 @@ while($row = $follow_result->fetch_row()) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
-<!--  Paper Kit Initialization snd functons -->
+<!-- Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
+
 </html>
