6d3740610ba61a2cf70aa41aaa3e78e075a7d928
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-13 18:48:30 -0500

    update

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -1,7 +1,6 @@
 <?php
-
 if (!isset($_SESSION)) {
-  session_start();
+    session_start();
 }
 require('dbconnection.php');
 
@@ -9,65 +8,67 @@ $userid = $_SESSION['user_id'];
 
 $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
 $result = $conn->query($sql);
-
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Profile Page by Andrew</title>
+    <title>Profile Page by Andrew</title>
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-        <!-- Bootstrap core CSS     -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
 
-    <!--     Fonts and icons     -->
-        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
-
+    <!-- Fonts and icons -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
         <div class="container">
-                        <div class="navbar-translate">
-                    <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-bar"></span>
-                                        <span class="navbar-toggler-bar"></span>
-                                        <span class="navbar-toggler-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Follow Me</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a href="login.php" class="nav-link">Login</a>
-                        </li>
-                 
-                 <li class="nav-item">
-                     <a href="profile.php" class="nav-link">Profile</a>
-                 </li>
-                 <li class="nav-item">
-                    <a href="editprofile.php" class="nav-link">Edit Profile</a>
-                </li>
-                <li class="nav-item">
-                     <a href="users.php" class="nav-link">Users</a>
-                 </li>
-                 
-                    </ul>
-                </div>
-                </div>
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Users</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="profile.php" class="nav-link">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="editprofile.php" class="nav-link">Edit Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="users.php" class="nav-link">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <?php echo $_SESSION['email']; ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <div class="wrapper">
@@ -120,7 +121,7 @@ $result = $conn->query($sql);
                                 <ul class="list-unstyled follows">
                                   <?php
 
-                                  $following_sql = "SELECT user_id FROM fm_followers WHERE following_user_id = '$userid'";
+                                  $followingSQL = "SELECT user_id FROM fm_followers WHERE following_user_id = '$userid'";
                                   $following_result = $conn->query($following_sql);
 
                                   while($row = $following_result->fetch_row()) {
@@ -160,8 +161,8 @@ $result = $conn->query($sql);
                               $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
                               $result = $conn->query($sql);
 
-                              $following_sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
-                              $following_result = $conn->query($following_sql);
+                              $followingSQL = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
+                              $following_result = $conn->query($followingSQL);
 
                               while($row = $following_result->fetch_row()) {
                                 $following_user_id[] = $row[0];
--- FollowMe/examples/users.php
+++ FollowMe/examples/users.php
@@ -73,33 +73,38 @@ while($row = $follow_result->fetch_row()) {
 
 </head>
 <body>
-  <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
-    <div class="container">
-                        <div class="navbar-translate">
-               <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                             <span class="navbar-toggler-bar"></span>
-                                   <span class="navbar-toggler-bar"></span>
-                                   <span class="navbar-toggler-bar"></span>
-               </button>
-               <a class="navbar-brand" href="#">Follow Me</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="login.php" class="nav-link">Login</a>
-                </li>
-               <li class="nav-item">
-                   <a href="follows.php" class="nav-link">Follow Users</a>
-               </li>
-               <li class="nav-item">
-                   <a href="profile.php" class="nav-link">Profile</a>
-               </li>
-               <li class="nav-item">
-                  <a href="editprofile.php" class="nav-link">Edit Profile</a>
-              </li>
-                    </ul>
-                </div>
-                </div>
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+        <div class="container">
+            <div class="navbar-translate">
+                <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                    <span class="navbar-toggler-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Users</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="profile.php" class="nav-link">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="editprofile.php" class="nav-link">Edit Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="users.php" class="nav-link">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <?php echo $_SESSION['email']; ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <div class="wrapper">
