ee4b50ce95cc7de805d99a69c5ff41682977792b
aripka55 <42806007+aripka55@users.noreply.github.com>
2018-11-14 11:37:12 -0500

    UPDATE

--- FollowMe/examples/profile.php
+++ FollowMe/examples/profile.php
@@ -1,7 +1,6 @@
 <?php
-
 if (!isset($_SESSION)) {
-  session_start();
+    session_start();
 }
 require('dbconnection.php');
 
@@ -9,37 +8,36 @@ $userid = $_SESSION['user_id'];
 
 $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
 $result = $conn->query($sql);
-
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-        <title>Profile Page by Andrew</title>
+    <title>Profile Page by Andrew</title>
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-        <!-- Bootstrap core CSS     -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-        <!--  CSS for Demo Purpose, don't include it in your project     -->
-        <link href="../assets/css/demo.css" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../assets/css/demo.css" rel="stylesheet" />
 
-    <!--     Fonts and icons     -->
-        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
-
+    <!-- Fonts and icons -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
-<nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
         <div class="container">
             <div class="navbar-translate">
                 <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -75,29 +73,23 @@ $result = $conn->query($sql);
 
     <div class="wrapper">
         <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-                        <div class="filter"></div>
-                </div>
+            <div class="filter">
+            </div>
+        </div>
         <div class="section profile-content">
             <div class="container">
                 <div class="owner">
                     <div class="avatar">
-
                         <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-
                     </div>
                     <div class="name">
-
                         <h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4>
-
-                                                <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
-
+                        <h6 class="description"><?php echo $_SESSION['title']; ?></h6>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 ml-auto mr-auto text-center">
-
                         <p><?php echo $_SESSION['description']; ?></p>
-
                         <br />
                         <btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
                     </div>
@@ -121,84 +113,76 @@ $result = $conn->query($sql);
                         <div class="row">
                             <div class="col-md-6 ml-auto mr-auto">
                                 <ul class="list-unstyled follows">
-                                  <?php
+                                    <?php
+                                    $following_sql = "SELECT user_id FROM fm_followers WHERE following_user_id = '$userid'";
+                                    $following_result = $conn->query($following_sql);
 
-                                  $following_sql = "SELECT user_id FROM fm_followers WHERE following_user_id = '$userid'";
-                                  $following_result = $conn->query($following_sql);
-
-                                  while($row = $following_result->fetch_row()) {
-                                    $user_ids[] = $row[0];
-                                  }
-
-                                  while($row = $result->fetch_assoc()) {
-
-                                    $follower_userid = $row['user_id'];
-
-                                    if (in_array($follower_userid, $user_ids)) {
-                                      echo "<li>";
-                                      echo      "<div class=\"row\">";
-                                      echo              "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-                                      echo                      "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                      echo              "</div>";
-                                      echo              "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-                                      echo                      "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                      echo              "</div>";
-                                      echo      "</div>";
-                                      echo "</li>";
-                                      echo "<hr />";
+                                    while($row = $following_result->fetch_row()) {
+                                        $user_ids[] = $row[0];
                                     }
-                                  }
-
-                                  ?>
+                                    while($row = $result->fetch_assoc()) {
+                                        $follower_userid = $row['user_id'];
+                                        if (in_array($follower_userid, $user_ids)) {
+                                            echo "<li>";
+                                            echo "<div class=\"row\">";
+                                            echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
+                                            echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
+                                            echo "</div>";
+                                            echo "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
+                                            echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
+                                            echo "</div>";
+                                            echo "</div>";
+                                            echo "</li>";
+                                            echo "<hr />";
+                                        }
+                                    }
+                                    ?>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div class="tab-pane text-center" id="following" role="tabpanel">
-                      <div class="row">
-                          <div class="col-md-6 ml-auto mr-auto">
-                            <ul class="list-unstyled follows">
-                              <?php
+                        <div class="row">
+                            <div class="col-md-6 ml-auto mr-auto">
+                                <ul class="list-unstyled follows">
+                                    <?php
+                                        $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+                                        $result = $conn->query($sql);
 
-                              $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
-                              $result = $conn->query($sql);
+                                        $following_sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
+                                        $following_result = $conn->query($following_sql);
 
-                              $following_sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$userid'";
-                              $following_result = $conn->query($following_sql);
+                                        while($row = $following_result->fetch_row()) {
+                                            $following_user_id[] = $row[0];
+                                        }
+                                        while($row = $result->fetch_assoc()) {
+                                            $user_id = $row['user_id'];
 
-                              while($row = $following_result->fetch_row()) {
-                                $following_user_id[] = $row[0];
-                              }
-
-                              while($row = $result->fetch_assoc()) {
-
-                                $user_id = $row['user_id'];
-
-                                if (in_array($user_id, $following_user_id)) {
-                                  echo "<li>";
-                                  echo  "<div class=\"row\">";
-                                  echo          "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
-                                  echo                  "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                  echo          "</div>";
-                                  echo          "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
-                                  echo                  "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                  echo          "</div>";
-                                  echo  "</div>";
-                                  echo "</li>";
-                                  echo "<hr />";
-                                }
-                              }
-
-                              ?>
-                          </ul>
+                                            if (in_array($user_id, $following_user_id)) {
+                                                echo "<li>";
+                                                echo "<div class=\"row\">";
+                                                echo "<div class=\"col-md-2 col-sm-2 ml-auto mr-0\">";
+                                                echo "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
+                                                echo "</div>";
+                                                echo "<div class=\"col-md-7 col-sm-4  ml-0 mr-0\">";
+                                                echo "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
+                                                echo "</div>";
+                                                echo "</div>";
+                                                echo "</li>";
+                                                echo "<hr />";
+                                            }
+                                        }
+                                    ?>
+                                </ul>
+                            </div>
                         </div>
-                      </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-        <footer class="footer section-dark">
+
+    <footer class="footer section-dark">
         <div class="container">
             <div class="row">
                 <nav class="footer-nav">
@@ -221,10 +205,10 @@ $result = $conn->query($sql);
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
 <!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
-
 
 <!--  Paper Kit Initialization snd functons -->
 <script src="../assets/js/paper-kit.js?v=2.1.0"></script>
